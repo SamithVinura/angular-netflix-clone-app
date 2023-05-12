3b7f273430f75912270ed62cc0b3aa262b9044ff
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-05-12 08:28:13 +0530

    load movie details in movie detail component functionality done

--- src/app/pages/movie-details/movie-details.component.ts
+++ src/app/pages/movie-details/movie-details.component.ts
@@ -1,4 +1,6 @@
 import { Component,OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieApiServiceService } from 'src/app/services/movie-api-service.service';
 
 @Component({
   selector: 'app-movie-details',
@@ -6,9 +8,21 @@ import { Component,OnInit } from '@angular/core';
   styleUrls: ['./movie-details.component.css']
 })
 export class MovieDetailsComponent implements OnInit {
+  getMovieDetailResult:any;
+  getMovieVideoResult:any;
+  getMovieCastResult:any;
 
-  constructor(){}
+  constructor(private movieApiSearvice:MovieApiServiceService,private router:ActivatedRoute){}
 
-  ngOnInit():void{}
+  ngOnInit():void{
+    let getParamId= this.router.snapshot.paramMap.get('id')
+    this.getMovie(getParamId)
+  }
+
+  getMovie(id:any){
+    this.movieApiSearvice.getMovieDetails(id).subscribe((res)=>{
+      this.getMovieDetailResult = res
+    })
+  }
 
 }
